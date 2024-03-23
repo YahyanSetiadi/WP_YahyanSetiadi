07ce52bea604b22b59711dd86f27f5f377e8adf8
Yahyan Setiadi <yahyan.nesya10@gmail.com>
2024-03-23 09:12:48 +0700

    menambahkan II

--- application/views/view-latihan1.php
+++ application/views/view-latihan1.php
@@ -4,7 +4,7 @@
     <title>Latihan 1</title>
 </head>
 <body>
-    Halo Kawan.. Yuk kita belajar web programming..!!!<br>
+    Halo Kawan.. Yuk kita belajar web programming II ..!!!<br>
     Nilai 1 = <?= $nilai1; ?> <br>
     Nilai 2 = <?= $nilai2; ?> <br>
     ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
